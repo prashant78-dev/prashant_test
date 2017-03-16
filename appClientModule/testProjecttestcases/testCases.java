78f0b08ab124a17f2f1d5db7824d0601bc04500e
ybs2015 <Prashant.sinh@yahoo.com>
2017-03-16 13:53:04 +0800

    Commit for Test proj

---
+++ appClientModule/testProjecttestcases/testCases.java
@@ -0,0 +1,15 @@
+package testProjecttestcases;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class testCases {
+
+	@Test
+	public void test() {
+		assertTrue("HELO"=="HELO");
+	}
+
+}
